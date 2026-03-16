2069cd3f8b762b0a712fff5faccf2dc76883df88
iamfrdfa <fri.faraji@gmail.com>
2026-03-16 13:11:25 +0100

    imported forsmodule

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,7 @@ import {
     ViewChild,
     ViewChildren,
 } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import {
     combineLatest,
@@ -63,7 +63,7 @@ interface MessageGroup {
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [CommonModule, ReactiveFormsModule, PickerComponent],
+    imports: [CommonModule, ReactiveFormsModule, PickerComponent, FormsModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
