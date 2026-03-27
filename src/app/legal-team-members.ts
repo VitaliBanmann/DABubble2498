34b11be1dafc0a1bc38762d1abe00535fd196d47
Danny Gruchmann <danny.grmn@icloud.com>
2026-03-27 16:32:46 +0100

    add name in legals

--- src/app/legal-team-members.ts
+++ src/app/legal-team-members.ts
@@ -7,4 +7,5 @@ export const LEGAL_TEAM_MEMBERS: LegalTeamMember[] = [
     { name: 'Vitali Banmann', email: 'vitali.banmann@gmail.com' },
     { name: 'Friedrich Faraji', email: 'fri.faraji@gmail.com' },
     { name: 'Ugursay Pürcek', email: 'ugursay.puercek@gmail.com' },
+    { name: 'Danny Gruchmann', email: 'dannygruchmann12345@gmail.com' },
 ];
